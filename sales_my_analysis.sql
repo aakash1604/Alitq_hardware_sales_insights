ac1684aa8e5224552af8e80ab04afc38fea06a1f
Aakash Sudanagunta <63837233+aakash1604@users.noreply.github.com>
2024-08-20 14:30:08 -0500

    Delete sales_my_analysis.sql

--- sales_my_analysis.sql
+++
@@ -1,4 +0,0 @@
-SELECT products.product_code,sum(transactions.sales_amount) 
-FROM transactions INNER JOIN products ON transactions.product_code=products.product_code
-group by products.product_code
-order by 2 desc
